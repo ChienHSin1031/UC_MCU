ba89ee96c1e11d6d0687556ab63640cef046ce30
ChienHsin <aa237914081301@gmail.com>
2020-09-06 13:05:36 +0800

    2020/09/06_init_code

--- UC_F5660_main.c
+++ UC_F5660_main.c
@@ -39,11 +39,13 @@ void init_HT16K33(void)
 	i2c_write(HT16K33_write);    //Tx 16k33 address
 	if(i2c_read_ACK() == 0) 
 	{
-		i2c_write(HT16K33_Turn_ON);   //start clock
+		
+		i2c_write(HT16K33_Display_ON);
 		if(i2c_read_ACK() == 0)
-			{
-				i2c_stop();
-			}
+		{
+			i2c_stop();
+		}
+			
 	}
 	
 	i2c_start();
@@ -53,35 +55,59 @@ void init_HT16K33(void)
 		i2c_write(HT16K33_Set_Row_Output);
 		if(i2c_read_ACK() == 0)
 		{
-			i2c_stop();	
+			i2c_stop();
 		}
-		
+	}
+	
+	i2c_start();
+	i2c_write(HT16K33_write);
+	if(i2c_read_ACK() == 0)
+	{	
+		i2c_write(HT16K33_Set_duty_16);
+		if(i2c_read_ACK() == 0)
+		{
+			i2c_stop();
+		}		
+	}
+	
+	
+	i2c_start();
+	i2c_write(HT16K33_write);
+	if(i2c_read_ACK() == 0)
+	{
+			i2c_write(HT16K33_Display_OFF);
+			if(i2c_read_ACK() == 0)
+			{
+				i2c_stop();
+			}	
 	}
 	
 	i2c_start();
 	i2c_write(HT16K33_write);
 	if(i2c_read_ACK() == 0)
 	{
-		i2c_write(HT16K33_Set_duty);
+		i2c_write(HT16K33_RAM);
 		if(i2c_read_ACK() == 0)
 		{
-			i2c_stop();	
+			int i =0;
+			for(i=0;i<12;i++)
+			{
+				if(i2c_read_ACK() == 1)
+				{
+					GCC_DELAY(100);
+				}
+				else
+				{
+					i2c_write(0xFF);
+				}
+			} 
+			i2c_stop();
+				
 		}
 		
 	}
 	
 	
-	i2c_start();
-	i2c_write(HT16K33_write);
-	if(i2c_read_ACK() == 0)
-	{
-		i2c_write(HT16K33_Display_OFF);
-		if(i2c_read_ACK() == 0)
-		{
-			i2c_stop();	
-		}
-		
-	}
 	
 }
 
@@ -99,16 +125,15 @@ void HT16K33_LED_ON()
 		i2c_write(0x00);
 		if(i2c_read_ACK() == 0)
 		{
-			i2c_write(0xFF);
-			if(i2c_read_ACK() == 0)
+			u8 i;
+			for(i=0;i<6;i++)
 			{
-				i2c_write(0x03);
+				i2c_write(0xFF);
 				if(i2c_read_ACK() == 0)
 				{
-					i2c_stop();
-				}		
-			}	
-			
+					i2c_write(0x03);
+				}	
+			}
 		}
 			
 	}
@@ -117,10 +142,14 @@ void HT16K33_LED_ON()
 	i2c_write(HT16K33_write);
 	if(i2c_read_ACK() == 0) 
 	{
-		i2c_write(HT16K33_Display_ON);
+		i2c_write(HT16K33_Display_ON_OFF);
 		if(i2c_read_ACK() == 0)
 		{
-			i2c_stop();	
+			i2c_write(HT16K33_Display_ON);
+			if(i2c_read_ACK() == 0)
+			{
+				i2c_stop();
+			}
 		}
 		
 	}
--- src/UC_I2C.h
+++ src/UC_I2C.h
@@ -8,14 +8,24 @@ void i2c_start(void);
 void i2c_stop(void);
 u8 i2c_read_ACK(void);
 void i2c_write(u8 i2c_buffer);
+u8 i2c_ACK_check(u8 ctrl_byte);
 
 //HT16K33 Command
 #define HT16K33_write 0b11100000
+#define HT16K33_Turn_ON_OFF 0x20
 #define HT16K33_Turn_ON 0x21
+
+#define HT16K33_Display_ON_OFF 0x80
 #define HT16K33_Display_OFF 0x80
 #define HT16K33_Display_ON 0x81
+
+#define HT16K33_Select_Row_INT 0xA0
 #define HT16K33_Set_Row_Output 0xA0
+
 #define HT16K33_Set_duty 0xEF
+#define HT16K33_Set_duty_16 0xEF
+
+#define HT16K33_RAM 0x00
 
 /***** I2C SCL select PC0 *****/
 #define	SCL			_pc0
